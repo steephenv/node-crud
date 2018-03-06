ebac4634fdf240feb64ec63d31c88584eebf8edf
steephenvrs3 <steephenvrs3@gmail.com>
2018-03-06 16:53:11 +0530

    Add PATCH/todos/:id

--- playground/server/server.js
+++ playground/server/server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -65,6 +67,32 @@ app.delete('/todos/:id',(req,res) =>{
   });
 });
 
+
+app.patch('/todos/:id',(req,res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }
+  else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo) =>{
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) =>{
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
